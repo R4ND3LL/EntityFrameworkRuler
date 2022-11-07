447b2d110c4f15fdda1d44dd87ea7cd50c899d29
R4ND3LL <65310488+R4ND3LL@users.noreply.github.com>
2022-11-07 15:20:08 -0330

    test project tweak

--- src/Tests/NorthwindTestProject/Program.cs
+++ src/Tests/NorthwindTestProject/Program.cs
@@ -2,15 +2,25 @@
 using NorthwindTestProject.Data;
 using NorthwindTestProject.Models;
 
+// ReSharper disable SuggestVarOrType_SimpleTypes
+// ReSharper disable PossibleNullReferenceException
+
 namespace NorthwindTestProject;
 
 internal class Program {
     private static void Main() {
+        /***************************************************************************************/
+        /********* Do not run this project.  It is a target for rule application only. *********/
+        /***************************************************************************************/
+        
         using var dbContext = new NorthwindDbContext(new DbContextOptions<NorthwindDbContext>());
 
         // ReSharper disable once SuggestVarOrType_SimpleTypes
         Order order = dbContext.Orders.FirstOrDefault();
         order.ShipVia = 1;
+        Product product = new Product();
+        Category category = product.CategoryIDNavigation;
+        var allCategoryProducts = category.ProductCategoryIDNavigations;
 
         Console.WriteLine("This is a fake test project to illustrate rule application only!");
     }
